ac896b912f4d469fad960ebd3e40fb9e65f09b52
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2023-12-28 21:49:42 +0700

    Update TESTTIME.java

--- TESTTIME.java
+++ TESTTIME.java
@@ -1,1 +1,9 @@
+package Exercise;
 
+public class TestTime {
+    public static void main(String[] args){
+        Time2 t1=new Time2(0, 30, 0);
+        Time2 t2=new Time2(t1);
+        System.out.printf("The time is %s\n", t2.ToUniversalString());
+    }
+}
