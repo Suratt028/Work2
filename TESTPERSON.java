7526f31d20f2fc1806a53bf43b72dbf53e81e29b
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2023-12-28 21:47:41 +0700

    Update TESTPERSON.java

--- TESTPERSON.java
+++ TESTPERSON.java
@@ -1,1 +1,12 @@
+package Jobby;
 
+public class TestPerson {
+    public static void main(String[] args){
+        Person person = new Person("Rus", "Tic","male", 21);
+        System.out.println(person);
+        Person person2 = new Person("Anna Lee", "Tic");
+        System.out.println(person2);
+        Person person3 = new Person("Anna Lee", "Tic","n/a", -1);
+        System.out.println(person3);
+    }
+}
